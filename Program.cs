23eab20e8a0b023246f481ad635821376cec1453
dbhullar <danny@bhullar.dk>
2020-07-17 01:53:15 +0200

    Updated

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using transport.ly.Models;
+using transport.ly.Services;
 
 namespace transport.ly
-{
+{    
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Tranport.ly");
+
+            // dependencies
+            IOrderService orderService = new OrderService();
+            IFleetService fleetService = new FleetService();
+
+            var inventoryManager = new InventoryManager(orderService, fleetService);
+
+            // User story 1
+            inventoryManager.ListFlightSchedule();
+
+            // User story 2
+            inventoryManager.GenerateFlightItineraries();
         }
     }
 }
